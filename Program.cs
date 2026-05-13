9331168302d0bbc0fdf7ad04be38340aabc3a58f
timaBujor <bujortimofei@gmail.com>
2026-05-13 14:50:45 +0300

    Lab 2 tasks with add comments in Readme file

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, Jerry");
+PrintText();
+PrintText();
+PrintText();
+
+void PrintText()
+{
+    Console.WriteLine("A");
+    Thread.Sleep(500);
+    Console.WriteLine("B");
+    Thread.Sleep(500);
+    Console.WriteLine("C");
+    Thread.Sleep(500);
+    PrintName();
+    PrintLastName();
+}
+
+void PrintName()
+{
+    Console.WriteLine("Tim");
+}
+
+void PrintLastName()
+{
+    Console.WriteLine("Bujor");
+}
--- obj/Debug/net10.0/lab_1.AssemblyInfo.cs
+++ obj/Debug/net10.0/lab_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e61efef942eb052e55bdb3fffb3f045c8e607c88")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
